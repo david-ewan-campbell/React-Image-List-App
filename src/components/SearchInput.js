e1a5e193823338adf15cfc7dea5766a8b88fd429
David Ewan Campbell <ewan.david@gmail.com>
2023-04-04 16:03:38 +0100

    SearchInput component changed to a controlled form so it stores it in the component not the DOM. Added values to show how they can be changed directly from the component i.e. adding an upper case method to the input

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,17 +1,21 @@
 import React from 'react';
 
 class SearchInput extends React.Component {
-  
+  state = { entry: 'hey...' }
+
   render() {
     return (
       <div className='ui segment' >
         <form className='ui form'>
           <div className='field'>
             <div className='ui massive icon input'>
-              <input type="text" placeholder='search...' 
-                  onChange={(event) => console.log(event.target.value)}
-                  /> 
-                <i className='search icon'></i>
+              <input 
+                type="text" 
+                placeholder='search...' 
+                onChange={(event) => this.setState({entry:event.target.value.toUpperCase()})}
+                value={this.state.entry}
+              /> 
+              <i className='search icon'></i>
             </div>
           </div>
         </form>
