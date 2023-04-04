724efcad5cca38c1db0762f4acf9d8ee50040e7b
David Ewan Campbell <ewan.david@gmail.com>
2023-04-04 16:58:14 +0100

    OnSubmit form added and constructor to add binding so state can be passed without causing an undefined error - 1st method

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,18 +1,28 @@
 import React from 'react';
 
 class SearchInput extends React.Component {
-  state = { entry: 'hey...' }
+  constructor() {
+    super()
+    this.onFormSubmit = this.onFormSubmit.bind(this)
+  }
+  
+  state = { entry: '' }
+  
+  onFormSubmit(event) {
+    event.preventDefault()
+    console.log(this.state.entry)
+  }
 
   render() {
     return (
       <div className='ui segment' >
-        <form className='ui form'>
+        <form onSubmit={this.onFormSubmit} className='ui form'>
           <div className='field'>
             <div className='ui massive icon input'>
               <input 
                 type="text" 
                 placeholder='search...' 
-                onChange={(event) => this.setState({entry:event.target.value.toUpperCase()})}
+                onChange={(event) => this.setState({entry:event.target.value})}
                 value={this.state.entry}
               /> 
               <i className='search icon'></i>
