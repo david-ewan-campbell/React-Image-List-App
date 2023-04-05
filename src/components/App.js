081924d942db9a5b8519c09d6b45bdaddcb8e9c7
David Ewan Campbell <ewan.david@gmail.com>
2023-04-05 12:02:13 +0100

    Axios package installed to assist with making API calls to pixabay - API key added with interpolation of user entry terms via our onSearchSubmit method so that images can be called from Pixabay - image data results are displaying when viewed in the network panel

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import axios from 'axios';
 import SearchInput from './SearchInput';
 
 class App extends React.Component {
 
   onSearchSubmit = (entry) => {
     console.log(entry)
-
+    axios.get(`https://pixabay.com/api/?key=35072712-f810da4dd90ca7208f438ef12&q=${entry}&image_type=photo&pretty=true`)
   }
 
 render() {
