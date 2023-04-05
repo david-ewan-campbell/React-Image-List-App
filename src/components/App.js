44d94945eff02859dd828c02538854d5ae7a39f3
David Ewan Campbell <ewan.david@gmail.com>
2023-04-05 14:16:25 +0100

    state object for images added to handle change of state when image searches are done and display the number of search results

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,20 @@ import SearchInput from './SearchInput';
 
 class App extends React.Component {
 
-  async onSearchSubmit(entry){
+  state = { images: [] }
+
+  onSearchSubmit = async (entry) => {
 
     const response = await axios.get(`https://pixabay.com/api/?key=35072712-f810da4dd90ca7208f438ef12&q=${entry}&image_type=photo&pretty=true`)
-    console.log(response.data.hits)
+
+    this.setState({images:response.data.hits})
 }
 
 render() {
   return (
     <div className='ui container' style={{marginTop:'50px'}} >
       <SearchInput onSearchSubmit={this.onSearchSubmit} />
+      We have { this.state.images.length } images
     </div>
     )
   }
